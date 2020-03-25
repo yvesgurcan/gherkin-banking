00f610e085331ce1a6d11106a64e2980c34dffac
yvesgurcan <gurcan.yves+github@gmail.com>
2020-03-25 16:57:22 -0700

    add 30 day expiration and cleanup

--- src/util.js
+++ src/util.js
@@ -2,15 +2,15 @@ import mongoose from 'mongoose';
 import { ObjectId } from 'mongodb';
 import AutoIncrement from 'mongoose-sequence';
 
-export function generateRandomNumber(length) {
+export function generateRandomNumber(desiredNumberLength) {
     if (process.env.TEST) {
-        return '1'.repeat(length);
+        return '1'.repeat(desiredNumberLength);
     }
 
-    const max = Number('9'.repeat(length));
-    const min = Number('1' + '0'.repeat(length - 1));
-    const numberWithinRange = Math.floor(Math.random() * (max - min) + min);
-    return numberWithinRange;
+    const max = Number('9'.repeat(desiredNumberLength));
+    const min = Number('1' + '0'.repeat(desiredNumberLength - 1));
+    const number = Math.floor(Math.random() * (max - min) + min);
+    return number;
 }
 
 export function castId() {
@@ -32,8 +32,10 @@ export function autoIncrement(schema, internalId) {
 
 export function generateExpirationDate() {
     if (process.env.TEST) {
-        return new Date('2021-01-01');
+        return new Date('2021-01-31');
     }
 
-    return new Date(); // add 1 month
+    const now = new Date();
+    now.setDate(now.getDate() + 30); // expires 30 days from today
+    return now;
 }
